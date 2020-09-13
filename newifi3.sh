da089a4f72eb711d639568264fa0b7153ff09cfa
kinddinosaur <56066587+kinddinosaur@users.noreply.github.com>
2020-09-13 14:46:03 +0800

    Update newifi3.sh

--- newifi3.sh
+++ newifi3.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 sed -i 's/#src-git helloworld https://github.com/fw876/helloworld/src-git helloworld https://github.com/fw876/helloworld/g' /feeds.conf.default
 
 #移除不用软件包
@@ -17,5 +17,5 @@ git clone https://github.com/cnzd/luci-app-koolproxyR.git package        #去广
 git clone https://github.com/xiaorouji/openwrt-package.git xiaorouji     #常用软件集合包
 git clone https://github.com/kenzok8/openwrt-packages.git                #常用软件集合包二
 git clone https://github.com/destan19/OpenAppFilter.git                  #APP过滤应用
-#git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
 
